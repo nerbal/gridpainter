f272f9b19a132ddc124527e2b22b3f3fa1ea2810
nerb <jacksondebroughe@outlook.com>
2024-06-29 00:43:36 +0930

    Updated Styles, added basic functionality for size changing

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const size = 16;
-
-const container = document.querySelector(".container");
-container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-console.log(`repeat(16, 1fr)`);
+let default_size = 16;
+const container = document.querySelector(".grid");
 
 
+function initializeGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    
+    for (let i = 0; i < (size * size); i++) {
+        const gridElement = document.createElement("div");
+        gridElement.style.backgroundColor = "cyan";
+        container.insertAdjacentElement("beforeend", gridElement);
+    }
+}
 
-for (let i = 0; i < (size * size); i++) {
-    const gridElement = document.createElement("div");
-    gridElement.style.backgroundColor = "blue";
-    container.insertAdjacentElement("beforeend", gridElement);
+
+initializeGrid(default_size)
+
+
+function changeSize(input) {
+    if(input >= 2 && input <= 128) {
+        initializeGrid(input);
+    }
 }
