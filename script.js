f4fb0a69baf2f739ad4ef8568dd52bbf1070f407
nerb <jacksondebroughe@outlook.com>
2024-06-29 16:24:57 +0930

    Adding some more styling & some minor renaming

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function initializeGrid(size) {
     container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
     
     for (let i = 0; i < (size * size); i++) {
-        const gridElement = document.createElement("div");
-        gridElement.style.backgroundColor = "bisque";
-        container.insertAdjacentElement("beforeend", gridElement);
+        const cell = document.createElement("div");
+        cell.style.backgroundColor = "bisque";
+        container.insertAdjacentElement("beforeend", cell);
     }
 }
 
@@ -20,5 +20,7 @@ initializeGrid(default_size)
 function changeSize(input) {
     if(input >= 2 && input <= 128) {
         initializeGrid(input);
+    } else {
+        alert("invalid amount!")
     }
 }
